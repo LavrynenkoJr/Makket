639c7c513943805c1931672d4641fa0f4b953cde
java-1-04 <Celestialapps@tutanota.com>
2017-01-20 17:17:43 +0200

    qqqq

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,52 @@
+import java.util.Random;
+
 public class Main {
 
+
+
+
+    static int x = 25;
     public static void main(String[] args) {
 
-        int[] a = {1, 2, 3, 4, 5};
+        Random r = new Random();
+
+        int[][] a = new int[3][];
+
+        a[0] = new int[3];
+        a[1] = new int[] {1, 2};
+        a[2] = new int[1];
+
+        boolean b = true;
+
+        for(int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
 
 
-        System.out.println(a[2]);
+        int min = 1;
+        int max = 100;
 
+        for (int i=0; i<26; i++){
+            int k = r.nextInt((max)+ min);
+            int n =r.nextInt((max) + min);
+            if (k<n)
+                System.out.println(k + " < " + n);
+            else if(k>n)
+                System.out.println(k + "  > " + n);
+            else
+                System.out.println(k + " = " + n);
+        }
+
+        //boolean b; break
+        int q = 0;
+        for(int i = 1; i<=1000; i++){
+            for (int k =2; k<100; k++){
+                q = i%k;
+                if(q==0&&q!=i){
+                    System.out.println(i);
+                }
+            }
+
+        }
     }
 }
